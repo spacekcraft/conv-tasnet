a1dce80e9383d1b1661a1ab4146cce1df76fdf48
Pavlus Jan <xpavlu10@merlin.fit.vutbr.cz>
2021-04-06 19:00:10 +0200

    Other evaluating methods added

---
+++ nnet/compute_si_snr_mom.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+
+# wujian@2018
+# xpavlu10@2020
+
+"""
+Compute SI-SDR as the evaluation metric
+"""
+
+import argparse
+
+from tqdm import tqdm
+
+from collections import defaultdict
+from libs.metric import si_snr, permute_si_snr, permute_si_snr_mix_of_mix, combine_si_snr_mix_of_mix
+from libs.audio import WaveReader, Reader
+
+from glob import glob
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import pickle
+
+import pdb
+
+class SpeakersReader(object):
+    def __init__(self, scps):
+        split_scps = scps.split(",")
+        if len(split_scps) == 1:
+            raise RuntimeError(
+                "Construct SpeakersReader need more than one script, got {}".
+                format(scps))
+        self.readers = [WaveReader(scp) for scp in split_scps]
+
+    def __len__(self):
+        first_reader = self.readers[0]
+        return len(first_reader)
+
+    def __getitem__(self, key):
+        return [reader[key] for reader in self.readers]
+
+    def __iter__(self):
+        first_reader = self.readers[0]
+        for key in first_reader.index_keys:
+            yield key, self[key]
+    
+    def keys(self):
+        first_reader = self.readers[0]
+        return first_reader.index_keys
+
+class Report(object):
+    def __init__(self, spk2gender=None, outputDir = None):
+        self.s2g = Reader(spk2gender) if spk2gender else None
+        self.snr = defaultdict(float)
+        self.cnt = defaultdict(int)
+        self.outputDir = outputDir
+        self.snrList = []
+    def add(self, key, val):
+        gender = "NG"
+        if self.s2g:
+            gender = self.s2g[key]
+        self.snr[gender] += val
+        self.cnt[gender] += 1
+        self.snrList.append(val)
+
+    def report(self):
+        print("SI-SDR(dB) Report: ")
+        for gender in self.snr:
+            tot_snrs = self.snr[gender]
+            num_utts = self.cnt[gender]
+            print("{}: {:d}/{:.3f}".format(gender, num_utts,
+                                           tot_snrs / num_utts))
+            if self.outputDir:
+                with open("{}/evalResult_{:.3f}.log".format(self.outputDir, tot_snrs/num_utts), "w") as outFile:
+                    outFile.write("{}: {:d}/{:.3f} SI-SNR".format(gender, num_utts,
+                                           tot_snrs / num_utts))
+                plt.figure()
+                plt.title("Histogram\n{}: {:d}/{:.3f} SI-SNR".format(gender, num_utts,
+                                           tot_snrs / num_utts))
+                plt.hist(self.snrList, bins = 30)
+                plt.savefig(self.outputDir+"/histogram.png")
+
+                with open(self.outputDir+"/snr.pkl",'wb') as f:
+                    pickle.dump(self.snrList, f)
+
+def run(args):
+    print("Working on folder {}".format(args.sep_scp))
+    #get all scp files from separation folder
+    folder = sorted(glob(args.sep_scp+'/*.scp', recursive=False)) # ndarray of names of all samples
+    sep_scp = ""
+    for scp in folder: # build string from array of scp files
+        sep_scp += scp + "," 
+    sep_scp = sep_scp[:-1] #remove last comma
+    
+    single_speaker = len(sep_scp.split(",")) == 1
+    reporter = Report(args.spk2gender, outputDir=args.sep_scp)
+
+    if single_speaker:
+        sep_reader = WaveReader(sep_scp)
+        ref_reader = WaveReader(args.ref_scp)
+        for key, sep in tqdm(sep_reader):
+            ref = ref_reader[key]
+            if sep.size != ref.size:
+                end = min(sep.size, ref.size)
+                sep = sep[:end]
+                ref = ref[:end]
+            snr = si_snr(sep, ref)
+            reporter.add(key, snr)
+    else:
+        sep_reader = SpeakersReader(sep_scp)
+        ref_reader = SpeakersReader(args.ref_scp)
+
+        for key, sep_list in tqdm(sep_reader):
+            #keys = key.split('=')
+            ref_list = ref_reader[key]# + ref_reader[keys[1]]
+            zero_ref_list = ref_reader[key]# + ref_reader[keys[1]]
+            if args.mixofmix != 0:
+                if len(ref_list) > len(sep_list):
+                    raise RuntimeError("There are more references then separs")
+                #create zero references
+                for i in range(len(sep_list) - len(ref_list)):
+                    zero_ref_list.append(np.zeros_like(ref_list[0])+0.0001)
+            
+            #Cut lengths
+            #Find if lenghts diffs
+            '''
+            diff = False
+            for i in range(len(sep_list)):
+               if sep_list[i].size != ref_list[i].size:
+                   diff = True
+                   break
+                '''
+            if sep_list[0].size != ref_list[0].size:
+                #find shortest signal
+                end = min(sep_list[0].size, ref_list[0].size)
+                #end = 999999999999
+                #for i in range(len(sep_list)):
+                #    end = min(min(sep_list[i].size, ref_list[i].size), end)
+                sep_list = [s[:end] for s in sep_list]
+                ref_list = [s[:end] for s in ref_list]
+                zero_ref_list = [s[:end] for s in zero_ref_list]
+            #if args.mixofmix != 0: # get right outputs combination
+            #    right_sep_list = permute_si_snr_mix_of_mix(sep_list, zero_ref_list)  
+            #else: right_sep_list = sep_list #compatibility
+            #PIT
+            snr = combine_si_snr_mix_of_mix(sep_list, ref_list)
+            reporter.add(key, snr)
+    reporter.report()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=
+        "Command to compute SI-SDR, as metric of the separation quality",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "sep_scp",
+        type=str,
+        help="Separated speech scripts, waiting for measure and also results output"
+        "(support multi-speaker, egs: spk1.scp,spk2.scp)")
+    parser.add_argument(
+        "ref_scp",
+        type=str,
+        help="Reference speech scripts, as ground truth for"
+        " SI-SDR computation")
+    parser.add_argument(
+        "--spk2gender",
+        type=str,
+        default="",
+        help="If assigned, report results per gender")
+    parser.add_argument(
+        "--mixofmix",
+        type=int,
+        default=0,
+        help="If not zero, use mixtures of mixtures evaluation")
+    args = parser.parse_args()
+    run(args)
---
+++ nnet/separate_mom.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+
+# wujian@2018
+# xpavlu10@2020
+
+import os
+import argparse
+import shutil
+import subprocess
+
+import torch as th
+import numpy as np
+
+from conv_tas_net import ConvTasNet
+
+from libs.utils import load_json, get_logger
+from libs.audio import WaveReader, write_wav
+
+import matplotlib.pyplot as plt
+from glob import glob
+
+from tqdm import tqdm
+import re
+import random
+
+import pdb
+
+logger = get_logger(__name__)
+
+logging = False
+
+class NnetComputer(object):
+    def __init__(self, cpt_dir, gpuid):
+        if gpuid >= 0:
+            #Nvidia smi call
+            freeGpu = subprocess.check_output('nvidia-smi -q | grep "Minor\|Processes"| grep "None" -B1 | tr -d " " | cut -d ":" -f2 | sed -n "1p"', shell=True)
+            if len(freeGpu) == 0: # if gpu not aviable use cpu
+                self.device = th.device("cpu")
+            else: self.device = th.device('cuda:'+freeGpu.decode().strip())
+        else:
+            self.device = th.device("cpu")
+        nnet = self._load_nnet(cpt_dir)
+        self.nnet = nnet.to(self.device) if gpuid >= 0 else nnet
+        # set eval model
+        self.nnet.eval()
+
+    def _load_nnet(self, cpt_dir):
+        nnet_conf = load_json(cpt_dir, "mdl.json")
+        nnet = ConvTasNet(**nnet_conf)
+        cpt_fname = os.path.join(cpt_dir, "best.pt.tar")
+        cpt = th.load(cpt_fname, map_location="cpu")
+        nnet.load_state_dict(cpt["model_state_dict"])
+        logger.info("Load checkpoint from {}, epoch {:d}".format(
+            cpt_fname, cpt["epoch"]))
+        return nnet
+
+    def compute(self, samps):
+        with th.no_grad():
+            raw = th.tensor(samps, dtype=th.float32, device=self.device)
+            sps = self.nnet(raw)
+            sp_samps = [np.squeeze(s.detach().cpu().numpy()) for s in sps]
+            return sp_samps
+
+#By xpavlu10
+def generateFile(pathToFolder, pathToFile):
+    print("Generating SCP", pathToFile, "for path:", pathToFolder)
+    fs = open(pathToFile, 'w')
+
+    folder = sorted(glob(pathToFolder+'/*.wav', recursive=False)) # ndarray of names of all samples
+    for i in folder:
+        fs.write(i.split('/')[-1].strip('.wav')+" "+i+"\n")
+    fs.close()
+    print("Generated")
+
+def plotOutputs(path, spks):
+    if logging is True: logger.info("Plot output {}".format(path))
+    #if dir doesn't exists then create
+    fdir = os.path.dirname(path)
+    if fdir and not os.path.exists(fdir):
+        os.makedirs(fdir)
+    
+    #get Mix and Min
+    max = 0
+    min = 0
+    for idx, samps in enumerate(spks):
+        newMax = np.max(samps)
+        if max < newMax: max = newMax
+        newMin = np.min(samps)
+        if min > newMin: min = newMin
+
+    #plot
+    plt.figure()
+    sl = np.ceil(len(spks)/2)
+    for idx, samps in enumerate(spks):
+        plt.subplot(sl, 2, idx+1)
+        plt.title("Speaker {}".format(idx+1))
+        plt.ylim(min-10, max+10)
+        plt.plot(samps)
+    plt.tight_layout()
+    plt.savefig(path)
+    plt.close()
+
+def cpyModelInfo(checkpoint, dump_dir):
+    os.mkdir(dump_dir+"/model_info")
+    files = glob(checkpoint+"/*.json")
+    for f in files:
+        shutil.copy(f, dump_dir+"/model_info/")
+
+def run(args):
+    os.mkdir(args.dump_dir)
+    mix_input = WaveReader(args.input, sample_rate=args.fs)
+    computer = NnetComputer(args.checkpoint, args.gpu)
+    cpyModelInfo(args.checkpoint, args.dump_dir)
+    lenGen = 0
+    keys = mix_input.keys()
+    for key, mix_samps in tqdm(mix_input):
+        #=========Get MoM===========
+        #get first and second speaker ID
+        firstSpeaker = key[:3]
+        secondSpeaker = key.split('_')[2][:3]
+        #get aviable keys - not contains first or second speaker from the first mix
+        aviableKeys = [key for key in keys if not re.search("((("+firstSpeaker+")|("+secondSpeaker+"))?.*_.*_(("+firstSpeaker+")|("+secondSpeaker+")).*)|((("+firstSpeaker+")|("+secondSpeaker+")).*_.*_(("+firstSpeaker+")|("+secondSpeaker+"))?.*)", key)] 
+        #randomly choose second key from aviable keys
+        secondKey = aviableKeys[random.randint(0,len(aviableKeys)-1)]
+        #get second mix
+        secondMix = mix_input[secondKey]
+        #Cut
+        if len(mix_samps) < len(secondMix):
+            secondMix = secondMix[:len(mix_samps)]
+        else:
+            secondMix = np.pad(secondMix, (0,len(mix_samps)-len(secondMix)), "constant",constant_values=(0,0))
+        #Mix
+        mix_samps = mix_samps+secondMix
+
+        if logging is True: logger.info("Compute on utterance {}...".format(key))
+        spks = computer.compute(mix_samps)
+        norm = np.linalg.norm(mix_samps, np.inf)
+        lenGen = len(spks)
+        for idx, samps in enumerate(spks):
+            samps = samps[:mix_samps.size]
+            # norm
+            samps = samps * norm / np.max(np.abs(samps))
+
+            #Saving separated speakers with name that contains first and second mixture key separated by '='
+            write_wav(
+                os.path.join(args.dump_dir, "spk{}/{}={}.wav".format(
+                    idx + 1, key, secondKey)),
+                samps,
+                fs=args.fs)
+        if args.plot != 0: plotOutputs(os.path.join(args.dump_dir, "plot_spk/{}.png".format(key)), spks)
+    #generate SCP files
+    for idx in range(lenGen):
+        generateFile(os.path.join(args.dump_dir, "spk{}".format(
+                    idx + 1)), os.path.join(args.dump_dir, "spk{}.scp".format(
+                    idx + 1)))
+    logger.info("Compute over {:d} utterances".format(len(mix_input)))
+
+def run2(args):
+    os.mkdir(args.dump_dir)
+    mix_input = WaveReader(args.input, sample_rate=args.fs)
+    computer = NnetComputer(args.checkpoint, args.gpu)
+    cpyModelInfo(args.checkpoint, args.dump_dir)
+    lenGen = 0
+    for key, mix_samps in tqdm(mix_input):
+        if logging is True: logger.info("Compute on utterance {}...".format(key))
+        
+        firstSpeaker = key[:3]
+        secondSpeaker = key.split('_')[2][:3]
+
+        aviableKeys = [key for key in mix_input.index_keys if not re.search("((("+firstSpeaker+")|("+secondSpeaker+"))?.*_.*_(("+firstSpeaker+")|("+secondSpeaker+")).*)|((("+firstSpeaker+")|("+secondSpeaker+")).*_.*_(("+firstSpeaker+")|("+secondSpeaker+"))?.*)", key)] 
+            
+        secondKey = aviableKeys[random.randint(0,len(aviableKeys)-1)]
+        secondMix = mix_input[secondKey]
+
+        if len(mix_samps) < len(secondMix):
+                secondMix = secondMix[:len(mix_samps)]
+        else:
+            secondMix = np.pad(secondMix, (0,len(mix_samps)-len(secondMix)), "constant",constant_values=(0,0))
+
+        mixofmix = mix_samps+secondMix
+
+        spks = computer.compute(mixofmix)
+        norm = np.linalg.norm(mixofmix, np.inf)
+        lenGen = len(spks)
+        for idx, samps in enumerate(spks):
+            samps = samps[:mixofmix.size]
+            # norm
+            samps = samps * norm / np.max(np.abs(samps))
+
+            write_wav(
+                os.path.join(args.dump_dir, "spk{}/{}.wav".format(
+                    idx + 1, key)),
+                samps,
+                fs=args.fs)
+        if args.plot != 0: plotOutputs(os.path.join(args.dump_dir, "plot_spk/{}.png".format(key)), spks)
+    #generate SCP files
+    for idx in range(lenGen):
+        generateFile(os.path.join(args.dump_dir, "spk{}".format(
+                    idx + 1)), os.path.join(args.dump_dir, "spk{}.scp".format(
+                    idx + 1)))
+    logger.info("Compute over {:d} utterances".format(len(mix_input)))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=
+        "Command to do speech separation in time domain using ConvTasNet",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("checkpoint", type=str, help="Directory of checkpoint")
+    parser.add_argument(
+        "--input", type=str, required=True, help="Script for input waveform")
+    parser.add_argument(
+        "--gpu",
+        type=int,
+        default=-1,
+        help="GPU device to offload model to, -1 means running on CPU")
+    parser.add_argument(
+        "--fs", type=int, default=8000, help="Sample rate for mixture input")
+    parser.add_argument(
+        "--dump-dir",
+        type=str,
+        default="sps_tas",
+        help="Directory to dump separated results out")
+    parser.add_argument(
+        "--plot",
+        type=int,
+        default=0,
+        help="If not 0, then plot results")
+    args = parser.parse_args()
+    run(args)
